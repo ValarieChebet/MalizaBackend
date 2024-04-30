d23be224bafdc8b7d07db79e225728f21718f49c
Okemwag <gabrielokemwa83@gmail.com>
2024-05-01 00:05:26 +0300

    Ready to deploy

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM python:3.11-alpine
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV PORT 8000
+
+
+# Install system dependencies specific to Alpine Linux
+RUN apk add --no-cache \
+    gcc \
+    g++ \
+    postgresql-client \
+    binutils \
+    proj-dev
+
+    
+# Create and set work directory called `app`
+WORKDIR /app
+
+# Install dependencies
+COPY requirements.txt .
+
+RUN set -ex && \
+    pip install --upgrade pip && \
+    pip install -r /app/requirements.txt 
+
+
+# Copy local project
+COPY . .
+
+# Expose port 8000
+EXPOSE $PORT
+
+
+CMD python manage.py runserver 0.0.0.0:$PORT
--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -71,11 +71,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     "default": {
+         "ENGINE": "django.db.backends.postgresql",
+         "NAME": "postgres",
+         "USER": "postgres",
+         "PASSWORD": "postgres",
+         "HOST": "db",  
+         "PORT": 5432,
+         "CONN_MAX_AGE": 15,
+         "CONN_HEALTH_CHECKS": True,  
+     }
+ }
 
 
 # Password validation
